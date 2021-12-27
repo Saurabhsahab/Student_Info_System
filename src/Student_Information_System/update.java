f5340994cfca67215abf21b1641ee777bacedf35
Saurabh <upadhyaysaurabh5960@gmail.com>
2021-12-28 02:01:34 +0530

    Bug Fixes
    !

--- src/Student_Information_System/update.java
+++ src/Student_Information_System/update.java
@@ -121,7 +121,9 @@ public class update extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        // TODO add your handling code here
+        
+        dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void txtPassActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPassActionPerformed
